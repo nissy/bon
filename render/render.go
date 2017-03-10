cb6e565935405e87af9e065399c762e721ce4b62
nissy <nishida@ngc224.org>
2017-03-10 12:54:30 +0900

    fix render json

--- render/render.go
+++ render/render.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 )
 
-func JSON(w http.ResponseWriter, status int, v interface{}) {
+func Json(w http.ResponseWriter, status int, v interface{}) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(status)
 	enc := json.NewEncoder(w)
@@ -19,7 +19,7 @@ func JSON(w http.ResponseWriter, status int, v interface{}) {
 	}
 }
 
-func BindJSON(r io.Reader, v interface{}) error {
+func BindJson(r io.Reader, v interface{}) error {
 	defer io.Copy(ioutil.Discard, r)
 	return json.NewDecoder(r).Decode(v)
 }
