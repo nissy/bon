296113b0b9a035d11504b19a1c10ad7c8a40f14b
nissy <nishida@ngc224.org>
2018-04-17 22:56:52 +0900

    test context

--- context_test.go
+++ context_test.go
@@ -7,22 +7,27 @@ import (
 	"testing"
 )
 
-var TestContextKey = &struct {
-	name string
-}{
-	name: "test",
-}
+var (
+	TestContextKeyAAA = &struct {
+		name string
+	}{
+		name: "AAA",
+	}
+	TestContextKeyBBB = &struct {
+		name string
+	}{
+		name: "BBB",
+	}
+)
 
-type ContextTest struct {
+type ContextValue struct {
 	value string
 }
 
-func ContextMiddleware(v string) func(next http.Handler) http.Handler {
+func ContextMiddleware(key, value interface{}) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		fn := func(w http.ResponseWriter, r *http.Request) {
-			r = r.WithContext(context.WithValue(r.Context(), TestContextKey, &ContextTest{
-				value: v,
-			}))
+			r = r.WithContext(context.WithValue(r.Context(), key, value))
 
 			next.ServeHTTP(w, r)
 		}
@@ -34,20 +39,37 @@ func ContextMiddleware(v string) func(next http.Handler) http.Handler {
 func TestContext(t *testing.T) {
 	r := NewRouter()
 
-	r.Use(ContextMiddleware("AAA"))
+	r.Use(ContextMiddleware(TestContextKeyAAA, &ContextValue{value: "AAA"}))
 	r.Get("/context1", func(w http.ResponseWriter, r *http.Request) {
-		v := r.Context().Value(TestContextKey).(*ContextTest)
+		v := r.Context().Value(TestContextKeyAAA).(*ContextValue)
 		w.Write([]byte(v.value))
 	})
 	r.Get("/context2/:vv", func(w http.ResponseWriter, r *http.Request) {
-		v := r.Context().Value(TestContextKey).(*ContextTest)
+		v := r.Context().Value(TestContextKeyAAA).(*ContextValue)
 		w.Write([]byte(v.value + URLParam(r, "vv")))
+	})
+	r.Use(ContextMiddleware(TestContextKeyBBB, &ContextValue{value: "BBB"}))
+	r.Get("/context3/:vv", func(w http.ResponseWriter, r *http.Request) {
+		a := r.Context().Value(TestContextKeyAAA).(*ContextValue)
+		b := r.Context().Value(TestContextKeyBBB).(*ContextValue)
+		w.Write([]byte(a.value + b.value + URLParam(r, "vv")))
+	})
+	r.Use(
+		ContextMiddleware(TestContextKeyAAA, &ContextValue{value: "DDD"}),
+		ContextMiddleware(TestContextKeyBBB, &ContextValue{value: "EEE"}),
+	)
+	r.Get("/context4/:vv", func(w http.ResponseWriter, r *http.Request) {
+		a := r.Context().Value(TestContextKeyAAA).(*ContextValue)
+		b := r.Context().Value(TestContextKeyBBB).(*ContextValue)
+		w.Write([]byte(a.value + b.value + URLParam(r, "vv")))
 	})
 
 	p := &Pattern{
 		Reqests: []*Reqest{
 			{"/context1", 200, "AAA"},
 			{"/context2/bbb", 200, "AAAbbb"},
+			{"/context3/ccc", 200, "AAABBBccc"},
+			{"/context4/fff", 200, "DDDEEEfff"},
 		},
 
 		Server: httptest.NewServer(r),
