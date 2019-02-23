15f22518f27f73a73b113048e23e3d6e0511ea8b
nissy <ni@sheeda.jp>
2019-02-24 03:54:18 +0900

    fix lint

--- middleware/cors.go
+++ middleware/cors.go
@@ -21,25 +21,20 @@ func CORS(cfg AccessControlConfig) func(next http.Handler) http.Handler {
 			if len(cfg.AllowOrigin) > 0 {
 				w.Header().Set("Access-Control-Allow-Origin", cfg.AllowOrigin)
 			}
-
 			if len(cfg.AllowMethods) > 0 {
 				w.Header().Set("Access-Control-Allow-Methods", strings.Join(cfg.AllowMethods, ","))
 			}
-
 			if len(cfg.AllowHeaders) > 0 {
 				w.Header().Set("Access-Control-Allow-Headers", strings.Join(cfg.AllowHeaders, ","))
 			}
-
 			if len(cfg.ExposeHeaders) > 0 {
 				w.Header().Set("Access-Control-Expose-Headers", strings.Join(cfg.ExposeHeaders, ","))
 			}
-
 			if cfg.MaxAge > 0 {
 				w.Header().Set("Access-Control-Max-Age", strconv.Itoa(cfg.MaxAge))
 			}
 
 			w.Header().Set("Access-Control-Allow-Credentials", strconv.FormatBool(cfg.AllowCredentials))
-
 			next.ServeHTTP(w, r)
 		}
 
